f8eb123b5c6b6c8945bb61c29544535b601ebed2
Louis OBERLE <louis.oberle@institutfrancais-chine.com>
2012-04-11 14:34:25 +0800

    bundle creation

---
+++ VElrteBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Venocl\VElrteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+* VElrteBundle
+*
+* @author Louis Oberlé <louisoberle@yahoo.fr>
+*/
+class VElrteBundle extends Bundle
+{
+}
