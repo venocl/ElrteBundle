1c87107b44209c9fe2412ef7b4665938dd5e5e26
Louis OBERLE <louis.oberle@institutfrancais-chine.com>
2012-04-18 15:22:08 +0800

    managed config

--- Form/Type/ElrteType.php
+++ Form/Type/ElrteType.php
@@ -3,6 +3,8 @@ namespace Venocl\ElrteBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
 
 /**
  * Form field using Elrte
@@ -11,9 +13,24 @@ use Symfony\Component\Form\FormBuilder;
  */
 class ElrteType extends AbstractType
 {
+    
+    private $options;
+
+    public function __construct(array $options)
+    {
+        $this->options = $options;
+    }
+
     public function getDefaultOptions(array $options)
     {
-        return array();
+        return array(
+            'required' => false,
+        );
+    }
+    
+    public function buildView(FormView $view, FormInterface $form)
+    {
+        $view->set('options', $this->options);
     }
 
     public function getParent(array $options)
